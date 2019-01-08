cfc30eabbcdbd2f4be9184c09214181ae1e5c871
susheng123 <1635811724@qq.com>
2019-01-08 09:28:03 +0800

    2019-01-08

--- ly_item/ly_item_service/src/main/java/com/leyou/LyItemService.java
+++ ly_item/ly_item_service/src/main/java/com/leyou/LyItemService.java
@@ -3,9 +3,14 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * http://localhost:10010/api/item/category/list?pid=0
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.item.mapper")
 public class LyItemService {
     public static void main(String[] args) {
         SpringApplication.run(LyItemService.class, args);
---
+++ ly_item/ly_item_service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -0,0 +1,10 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.pojo.Category;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * Created by susheng on 2018/10/17.
+ */
+public interface CategoryMapper extends Mapper<Category> {
+}
